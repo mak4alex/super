9264715dede8534b3434993d157521ebb19cb812
Alexander Maksimovich <mak4alex@gmail.com>
2015-10-10 10:31:08 +0200

    Add output array function

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        outputArray();
+    }
+
+    private static void outputArray() {
+        double[] items = new double[]{2.3, 4.5, 7.5, 5.3};
+
+        for (double item : items) {
+            System.out.println("Item: " + item);
+        }
+
     }
 }
