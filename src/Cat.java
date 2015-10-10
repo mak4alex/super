2e5e71880af0ae2e60aa724b66ca65f25bc30d59
Alexander Maksimovich <mak4alex@gmail.com>
2015-10-10 11:15:54 +0200

    Delete cat setters and getters

--- src/Cat.java
+++ src/Cat.java
@@ -12,21 +12,7 @@ public class Cat {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     @Override
     public String toString() {
