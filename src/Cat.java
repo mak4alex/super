9f61339676c98c8fc8c3b65ec448da9a2aa818f7
Alexander Maksimovich <mak4alex@gmail.com>
2015-10-10 10:32:58 +0200

    Add cat class

---
+++ src/Cat.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Compik on 10.10.2015.
+ */
+public class Cat {
+
+    String name;
+
+    int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
